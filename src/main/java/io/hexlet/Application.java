029c34ff3ec18e07a00e280cb67ee5819ecb3408
VictorGotsenko <victor.gotsenko@gmail.com>
2025-02-26 19:38:47 +0500

    Rewrite with: preparedStatement

--- src/main/java/io/hexlet/Application.java
+++ src/main/java/io/hexlet/Application.java
@@ -2,6 +2,7 @@ package io.hexlet;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Application {
     public static void main(String[] args) throws SQLException {
@@ -16,19 +17,62 @@ public class Application {
                 statement.execute(sql);
             }
 
-            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-            try (var statement2 = conn.createStatement()) {
-                statement2.executeUpdate(sql2);
+            var sql2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var preparedStatement = conn.prepareStatement(sql2)) {
+                preparedStatement.setString(1, "Tommy");
+                preparedStatement.setString(2, "+792145");
+                preparedStatement.executeUpdate();
+
+                preparedStatement.setString(1, "Maria");
+                preparedStatement.setString(2, "+792578");
+                preparedStatement.executeUpdate();
             }
 
-            var sql3 = "SELECT * FROM users";
+            var sql3 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var preparedStatement = conn.prepareStatement(sql3, Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, "Sarah");
+                preparedStatement.setString(2, "+792791");
+                preparedStatement.executeUpdate();
+                // Если ключ составной, значений может быть несколько
+                // В нашем случае, ключ всего один
+                var generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    System.out.println("added Record number = " + generatedKeys.getLong(1) + "\n");
+                } else {
+                    throw new SQLException("DB have not returned an id after saving the entity");
+                }
+            }
+
+
+            var sql5 = "SELECT * FROM users";
             try (var statement3 = conn.createStatement()) {
-                var resultSet = statement3.executeQuery(sql3);
+                var resultSet = statement3.executeQuery(sql5);
+                System.out.println("Print table Users:");
                 while (resultSet.next()) {
-                    System.out.println(resultSet.getString("username"));
+                    System.out.print(resultSet.getString("username") + "  ");
+                    System.out.println(resultSet.getString("phone"));
+                }
+                System.out.println();
+            }
+
+
+            var sql4 = "DELETE FROM users WHERE username = ?";
+            try (var preparedStatement = conn.prepareStatement(sql4)) {
+                String userName = "Maria";
+                preparedStatement.setString(1, userName);
+                preparedStatement.executeUpdate();
+                System.out.println("Delete user " + userName + "\n");
+            }
+
+            try (var statement3 = conn.createStatement()) {
+                var resultSet = statement3.executeQuery(sql5);
+                System.out.println("Print table Users:");
+                while (resultSet.next()) {
+                    System.out.print(resultSet.getString("username") + "  ");
                     System.out.println(resultSet.getString("phone"));
                 }
             }
+
         }
     }
 }
